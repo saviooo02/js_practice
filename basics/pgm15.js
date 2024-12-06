c4c286ca8e10ba003fa1c0f7e6c498086b44a6a0
Savio Shaji <savioshaji02@gmail.com>
2024-12-06 21:54:17 +0530

    edit

--- basics/pgm15.js
+++ basics/pgm15.js
@@ -3,12 +3,14 @@
 input = 7;
 flag = 1;
 
-for (i = 2; i <= input; i++) {
-  if (i % input == 0) {
+for (i = 2; i < input; i++) {
+  if (input % i == 0) {
     flag = 0;
   }
 }
 
 if (flag == 1) {
   console.log("prime");
+} else {
+  console.log("Not Prime");
 }
