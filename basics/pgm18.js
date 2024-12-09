8ecfbb3466fbf70d6ea515583e8374b56ed1c086
Savio Shaji <savioshaji02@gmail.com>
2024-12-09 11:14:41 +0530

    pattern questions

---
+++ basics/pgm18.js
@@ -0,0 +1,14 @@
+//print :
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+
+for (row = 1; row <= 4; row++) {
+  str = " ";
+
+  for (col = 1; col <= 4; col++) {
+    str = str + "# ";
+  }
+  console.log(str);
+}
