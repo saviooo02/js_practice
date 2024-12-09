f12a9a6426faf95d6236c6f7a5ef8b0dd4fb5875
Savio Shaji <savioshaji02@gmail.com>
2024-12-09 11:31:56 +0530

    pattern questions

--- basics/pgm25.js
+++ basics/pgm25.js
@@ -3,11 +3,13 @@
 //    * * *
 //   * * * *
 
-for(i=1;i<=4;i++){
-    str = " "
-    for(j=1<=4;j++){
-        for(k=4;k<=1;k--){
-            str = 
-        }
-    }
+for (i = 1; i <= 4; i++) {
+  str = " ";
+  for (sp = 3; sp >= i; sp--) {
+    str = str + " ";
+  }
+  for (j = 1; j <= i; j++) {
+    str = str + "* ";
+  }
+  console.log(str);
 }
