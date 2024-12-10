105a007955ab91738cddc315889668796a1a91b1
Savio Shaji <savioshaji02@gmail.com>
2024-12-10 08:34:11 +0530

    add pattern pgm

--- basics/pgm24.js
+++ basics/pgm24.js
@@ -7,3 +7,18 @@
 //* * *
 //* *
 //*
+
+for (i = 1; i <= 5; i++) {
+  str = " ";
+  for (j = 1; j <= i; j++) {
+    str = str + "* ";
+  }
+  console.log(str);
+}
+for (i = 1; i <= 4; i++) {
+  str = " ";
+  for (j = 4; j >= i; j--) {
+    str = str + "* ";
+  }
+  console.log(str);
+}
